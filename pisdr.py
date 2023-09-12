410aac440176448846ceb77239c753a36a303782
JoshuaSnow3 <joshuasnow.biz@yahoo.com>
2023-09-11 23:36:41 -0400

    Trying to fix the FFT

--- pisdr.py
+++ pisdr.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Not titled yet
+# Title: SDR
 # GNU Radio version: 3.10.1.1
 
 from packaging.version import Version as StrictVersion
@@ -21,10 +21,14 @@ if __name__ == '__main__':
             print("Warning: failed to XInitThreads()")
 
 from PyQt5 import Qt
+from PyQt5.QtCore import QObject, pyqtSlot
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio.fft import window
 import sys
@@ -34,6 +38,8 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
+import osmosdr
+import time
 
 
 
@@ -42,9 +48,9 @@ from gnuradio import qtgui
 class pisdr(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        gr.top_block.__init__(self, "SDR", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
+        self.setWindowTitle("SDR")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -75,41 +81,112 @@ class pisdr(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.freq = freq = 500
+        self.freq = freq = 105.5e6
         self.variable_qtgui_range_0 = variable_qtgui_range_0 = freq
-        self.samp_rate = samp_rate = 32000
+        self.source_chooser = source_chooser = '0'
+        self.samp_rate = samp_rate = 2048000
 
         ##################################################
         # Blocks
         ##################################################
-        self._variable_qtgui_range_0_range = Range(1, 10000, 10, freq, 200)
+        self._variable_qtgui_range_0_range = Range(86e6, 110e6, 10000, freq, 200)
         self._variable_qtgui_range_0_win = RangeWidget(self._variable_qtgui_range_0_range, self.set_variable_qtgui_range_0, "'variable_qtgui_range_0'", "counter_slider", float, QtCore.Qt.Horizontal)
         self.top_layout.addWidget(self._variable_qtgui_range_0_win)
+        # Create the options list
+        self._source_chooser_options = ['0', '1']
+        # Create the labels list
+        self._source_chooser_labels = ['FM', 'Test Signals']
+        # Create the combo box
+        # Create the radio buttons
+        self._source_chooser_group_box = Qt.QGroupBox("Source" + ": ")
+        self._source_chooser_box = Qt.QVBoxLayout()
+        class variable_chooser_button_group(Qt.QButtonGroup):
+            def __init__(self, parent=None):
+                Qt.QButtonGroup.__init__(self, parent)
+            @pyqtSlot(int)
+            def updateButtonChecked(self, button_id):
+                self.button(button_id).setChecked(True)
+        self._source_chooser_button_group = variable_chooser_button_group()
+        self._source_chooser_group_box.setLayout(self._source_chooser_box)
+        for i, _label in enumerate(self._source_chooser_labels):
+            radio_button = Qt.QRadioButton(_label)
+            self._source_chooser_box.addWidget(radio_button)
+            self._source_chooser_button_group.addButton(radio_button, i)
+        self._source_chooser_callback = lambda i: Qt.QMetaObject.invokeMethod(self._source_chooser_button_group, "updateButtonChecked", Qt.Q_ARG("int", self._source_chooser_options.index(i)))
+        self._source_chooser_callback(self.source_chooser)
+        self._source_chooser_button_group.buttonClicked[int].connect(
+            lambda i: self.set_source_chooser(self._source_chooser_options[i]))
+        self.top_layout.addWidget(self._source_chooser_group_box)
+        self.rtlsdr_source_0 = osmosdr.source(
+            args="numchan=" + str(1) + " " + ""
+        )
+        self.rtlsdr_source_0.set_time_unknown_pps(osmosdr.time_spec_t())
+        self.rtlsdr_source_0.set_sample_rate(samp_rate)
+        self.rtlsdr_source_0.set_center_freq(variable_qtgui_range_0, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(40, 0)
+        self.rtlsdr_source_0.set_if_gain(20, 0)
+        self.rtlsdr_source_0.set_bb_gain(20, 0)
+        self.rtlsdr_source_0.set_antenna('', 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+        self.rational_resampler_xxx_1_0_0 = filter.rational_resampler_fff(
+                interpolation=50,
+                decimation=50,
+                taps=[],
+                fractional_bw=0)
+        self.rational_resampler_xxx_1_0 = filter.rational_resampler_ccf(
+                interpolation=1,
+                decimation=4,
+                taps=[],
+                fractional_bw=0)
         self.qtgui_sink_x_0 = qtgui.sink_c(
             1024, #fftsize
             window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
+            variable_qtgui_range_0, #fc
+            100e6, #bw
             "", #name
             True, #plotfreq
             True, #plotwaterfall
-            True, #plottime
-            True, #plotconst
+            False, #plottime
+            False, #plotconst
             None # parent
         )
         self.qtgui_sink_x_0.set_update_time(1.0/10)
         self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
 
-        self.qtgui_sink_x_0.enable_rf_freq(False)
+        self.qtgui_sink_x_0.enable_rf_freq(True)
 
         self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_SIN_WAVE, variable_qtgui_range_0, 1, 0, 0)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            1,
+            firdes.low_pass(
+                1,
+                samp_rate,
+                100e3,
+                1e6,
+                window.WIN_HAMMING,
+                6.76))
+        self.blocks_multiply_const_vxx_0_0 = blocks.multiply_const_ff(1)
+        self.audio_sink_0_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=500e3,
+        	audio_decimation=10,
+        )
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_1_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0_0, 0), (self.audio_sink_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.rational_resampler_xxx_1_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.rational_resampler_xxx_1_0_0, 0), (self.blocks_multiply_const_vxx_0_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.rational_resampler_xxx_1_0, 0))
 
 
     def closeEvent(self, event):
@@ -132,15 +209,23 @@ class pisdr(gr.top_block, Qt.QWidget):
 
     def set_variable_qtgui_range_0(self, variable_qtgui_range_0):
         self.variable_qtgui_range_0 = variable_qtgui_range_0
-        self.analog_sig_source_x_0.set_frequency(self.variable_qtgui_range_0)
+        self.qtgui_sink_x_0.set_frequency_range(self.variable_qtgui_range_0, 100e6)
+        self.rtlsdr_source_0.set_center_freq(self.variable_qtgui_range_0, 0)
+
+    def get_source_chooser(self):
+        return self.source_chooser
+
+    def set_source_chooser(self, source_chooser):
+        self.source_chooser = source_chooser
+        self._source_chooser_callback(self.source_chooser)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 100e3, 1e6, window.WIN_HAMMING, 6.76))
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
 
 
 
